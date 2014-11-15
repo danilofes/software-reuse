cfdade7b60c964f9951ec91e49569ace93e111c5
Danilo F. Silva <danilofes@gmail.com>
2014-11-15 00:20:35 -0200

    Documentação.

---
+++ sege/features/simules/sege/simules/SimulEs.java
@@ -0,0 +1,16 @@
+package sege.simules;
+
+import java.util.List;
+
+import sege.GamePlugin;
+import sege.InternalGameState;
+import sege.quiz.Question;
+import sege.quiz.QuestionsPool;
+
+public class SimulEs implements GamePlugin {
+
+	public InternalGameState createInitialState(List<String> players) {
+		return null;
+	}
+
+}
